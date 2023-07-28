d753070f90b9f6517642a3aa1bf6520bb397d165
Terence Tong <terencet@Terences-MacBook-Pro.local>
2023-07-27 17:19:31 -0700

    dayoftheweek function

--- day_of_week/day_of_week.go
+++ day_of_week/day_of_week.go
@@ -10,6 +10,8 @@ Return the answer as one of the following values {"Sunday", "Monday", "Tuesday",
 
 The given dates are valid dates between the years 1971 and 2100.
 
+definitely would not be able to do this if asked in an interview bc i can't even understand it when i have the time to look at it
+
 */
 import "time"
 
@@ -19,44 +21,44 @@ func dayOfTheWeek(day int, month int, year int) string {
 	DayOfWeek := today.Weekday()
 	return DayOfWeek.String()
 
-	/*
-			currently not working, i'm not sure why
-		    // start this list with today's date
-		    dayNames := [7]string{"Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"}
-		    daysInMonth := [12]int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	// this solution is kinda hard
+	/* revolves around using the knowledge of todays date
+	daysInMonth := [12]int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 
-		    leapYearOffset := func(year int) int {
-		        if (year % 4 == 0 && year % 100 != 0) || year % 400 != 0 {
-		            return 1
-		        }
-		        return 0
-		    }
+	leapYearOffset := func(year int) int {
+		if (year%4 == 0 && year%100 != 0) || year%400 == 0 {
+			return 1
+		}
+		return 0
+	}
 
-		    daysSinceStart := func(d, m, y int) int {
-		        // start is 31 Dec 1970
-		        days := 0
+	daysSinceStart := func(d, m, y int) int {
+		// start is 31 Dec 1970
+		days := 0
 
-		        // years
-		        for i := 1971; i < y; i++ {
-		            days += 365 + leapYearOffset(y)
-		        }
+		// years
+		for i := 1971; i < y; i++ {
+			days += 365 + leapYearOffset(y)
+		}
 
-		        // months
-		        for i := 1; i < m - 1; i++ {
-		            days += daysInMonth[i-1]
-		        }
+		// months
+		for i := 1; i < m; i++ {
+			days += daysInMonth[i-1]
+		}
 
-		        // days
-		        if month > 1 {
-		            days += leapYearOffset(y)
-		        }
-		        days += d
-		        return days
-		    }
+		// days
+		if month > 2 {
+			days += leapYearOffset(y)
+		}
+		days += d
+		return days
+	}
 
-		    // make sure dayNames starts with the day it is below
-		    knownDateDifference := daysSinceStart(27, 8, 2023)
-		    d := daysSinceStart(day, month, year)
-		    return dayNames[((d - knownDateDifference) % 7 + 7) % 7]
+	// make sure this date day is 0-index in dayNames
+	knownDate := daysSinceStart(27, 8, 2023)
+	d := daysSinceStart(day, month, year)
+	// start this list with today's date
+	dayNames := [7]string{"Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"}
+	return dayNames[((d-knownDate)%7+7)%7]
 	*/
 }
